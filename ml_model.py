0749ae17a14563affb824b8e0afec41e3b5d7ff2
Sharedbyamaresh <amaresh2394@gmail.com>
2022-08-31 21:24:50 +0530

    Added new 3 models

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,11 @@
 print('Adding all ML related Code')
+
+
+def model1():
+    print("Code for model 1")
+
+def model2():
+    print("Code for model 2")
+
+def model3():
+    print('Code for model 3')
